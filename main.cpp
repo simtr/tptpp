3be62bffe3e618fdcb34a67c762fa8236e561971
Phil <cracker642@gmail.com>
2011-07-14 11:42:29 -0400

    Random text that moves around.. yay text works.

--- main.cpp
+++ main.cpp
@@ -21,14 +21,26 @@ int main(int argc, char** argv){
 		App.Draw(sf::Shape::Line(10, 10, 710, 100, 15, sf::Color::Red));
 		App.Draw(sf::Shape::Circle(200, 200, 100, sf::Color::Yellow, 10, sf::Color::Blue));
 		App.Draw(sf::Shape::Rectangle(350, 200, 600, 350, sf::Color::Green));
-		//i get linker errors for text...
-		/*sf::String String = "The Powder Toy";
+
+		//random bouncy text
+		static float textX = 100.0f;
+		static float textY = 150.0f;
+		static int changeX = 1;
+		static int changeY = 1;
+		textX += changeX;
+		textY += changeY;
+		if (textX > 600) changeX = -1;
+		if (textY > 500) changeY = -1;
+		if (textX <= 0) changeX = 1;
+		if (textY <= 0) changeY = 1;
+
+		sf::String String = "The Powder Toy";
 		sf::Text Text(String);
 		Text.SetColor(sf::Color(128, 128, 0));
-		Text.SetRotation(90.f);
-		Text.SetScale(2.f, 2.f);
-		Text.Move(100.f, 200.f);
-		*/ 
+		Text.Move(textX,textY);
+
+		App.Draw(Text);
+
 		//Draw everything
 		App.Display();
 	}
