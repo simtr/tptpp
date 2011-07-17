096e0b0d989a27ed6f56d97c0e190b29eefd3616
Bryan Hoyle <starfoxprime@gmail.com>
2011-07-17 16:00:30 -0400

    Global function: 'CreatePart' arguments fix

--- src/GlobalFunctions.hpp
+++ src/GlobalFunctions.hpp
@@ -1,5 +1,5 @@
 #ifndef _GLOBAL_FUNCTIONS
 #define _GLOBAL_FUNCTIONS
 #include "Layer.hpp"
-template<class T>void CreatePart(Layer& layer,int x,int y);
+template<class T>void CreatePart(Layer& layer,int x,int y, int type);
 #endif
