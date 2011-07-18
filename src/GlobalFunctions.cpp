5975dcc4b6c8fefb25075d419ae641a84c46351a
Bryan Hoyle <starfoxprime@gmail.com>
2011-07-18 07:18:42 -0400

    Added some more functions and variables to the 'Particle' class

--- src/GlobalFunctions.cpp
+++ src/GlobalFunctions.cpp
@@ -6,6 +6,7 @@ template<class T>void CreatePart(Layer& layer,int x,int y,int type){
 for(int i =0; i < layer.MaxParts;i++)
     if(layer.Parts[i].Type==0){
         layer.Parts[i].ChangeType<T>(type);
+        layer.Parts[i].SetPos(x,y,layer.PartTable);
     }
 }
 void CreatePartByLookup(Layer& layer,int x,int y,int type){
